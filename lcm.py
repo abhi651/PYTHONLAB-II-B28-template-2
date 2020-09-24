665f327ad68d825d71bc43a0c749f8a30c2eb0ad
abhi-651 <akumar2@gitam.edu>
2020-09-24 11:25:17 +0530

    Done

--- lcm.py
+++ lcm.py
@@ -5,7 +5,18 @@ import unittest
 
 
 def lcm(num1, num2):
-    pass
+    # Choose the greatest number
+    if num1 > num2:
+        greater = num1
+    else:
+        greater = num2
+
+    while(True): 
+        if greater % num1 == 0 and greater % num2 == 0:
+            lcm_compute = greater
+            break
+        greater = greater + 1
+    return lcm_compute 
 
 
 
